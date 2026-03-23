1bbdbabff4d26322a07ecd40219d63a9f0e5a197
Deva97 <54913203+Deva97@users.noreply.github.com>
2026-03-23 08:35:21 +0000

    Setup TcpListener and TcpClient for port 6379. 1. Implement server response PONG

---
+++ src/ServerTest/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+
+Console.WriteLine("server test started");
+var ipAddress = IPAddress.Parse("127.0.0.1");
+using var client = new TcpClient();
+
+async Task sendData(NetworkStream? stream)
+{
+    var message = "+PING\r\n";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    await stream.WriteAsync(messageBytes);
+}
+
+async Task readData(NetworkStream? stream)
+{
+    var buffer = new byte[1_024];
+      var data = await stream.ReadAsync(buffer);
+      var strdata = Encoding.UTF8.GetString(buffer, 0 , data);
+      System.Console.WriteLine(strdata);
+}
+
+try
+{
+
+    //connect -> set stream -> readData
+    await client.ConnectAsync(ipAddress, 6379);
+    System.Console.WriteLine("server status " + $"{client.Connected}");
+
+     await using var stream = client.GetStream();
+     await sendData(stream);
+     await readData(stream);
+     
+}
+catch(Exception e)
+{
+    System.Console.WriteLine(e);
+}
+finally
+{
+}
+
+
+//1.Ping test to TCP 63
+
---
+++ src/sillyredis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var server = new TcpListener(ipAddress, 6379);
+
+try
+{
+    server.Start();
+    var source = new CancellationTokenSource();
+    var token = source.Token;
+    using var acceptClinet = await server.AcceptTcpClientAsync(token);
+    await using var stream =  acceptClinet.GetStream();
+    var message = "+PONG\r\n";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+   
+    var buffer = new byte[1024];
+    while((await stream.ReadAsync(buffer)) > 0)
+    await stream.WriteAsync(messageBytes, cancellationToken: token);
+server.AcceptSocket();
+}
+catch(Exception e)
+{
+    
+}
+finally
+{
+    server.Stop();
+}
